b8f2b6b24f99dba818d5e81d5b7119a4679cce95
Kjell Hopkins <91137333+FIL3SAV3D@users.noreply.github.com>
2025-02-25 17:24:57 +0100

    Experimental Jolt Test

--- src/ArcEngine/Temp/FirstApp.cpp
+++ src/ArcEngine/Temp/FirstApp.cpp
@@ -21,7 +21,154 @@
 #include <array>
 #include <chrono>
 
+// Jolt
 #include <Jolt/Jolt.h>
+
+// Jolt includes
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+#include <iostream>
+
+using namespace JPH;
+
+namespace Layers
+{
+	static constexpr JPH::ObjectLayer NON_MOVING = 0;
+	static constexpr JPH::ObjectLayer MOVING = 1;
+	static constexpr JPH::ObjectLayer NUM_LAYERS = 2;
+};
+
+namespace BroadPhaseLayers
+{
+	static constexpr JPH::BroadPhaseLayer NON_MOVING(0);
+	static constexpr JPH::BroadPhaseLayer MOVING(1);
+	static constexpr JPH::uint NUM_LAYERS(2);
+};
+
+class ObjectLayerPairFilterImpl : public ObjectLayerPairFilter
+{
+public:
+	virtual bool					ShouldCollide(ObjectLayer inObject1, ObjectLayer inObject2) const override
+	{
+		switch (inObject1)
+		{
+		case Layers::NON_MOVING:
+			return inObject2 == Layers::MOVING; // Non moving only collides with moving
+		case Layers::MOVING:
+			return true; // Moving collides with everything
+		default:
+			JPH_ASSERT(false);
+			return false;
+		}
+	}
+};
+
+class BPLayerInterfaceImpl final : public JPH::BroadPhaseLayerInterface
+{
+public:
+	BPLayerInterfaceImpl()
+	{
+		// Create a mapping table from object to broad phase layer
+		mObjectToBroadPhase[Layers::NON_MOVING] = BroadPhaseLayers::NON_MOVING;
+		mObjectToBroadPhase[Layers::MOVING] = BroadPhaseLayers::MOVING;
+	}
+
+	virtual JPH::uint					GetNumBroadPhaseLayers() const override
+	{
+		return BroadPhaseLayers::NUM_LAYERS;
+	}
+
+	virtual JPH::BroadPhaseLayer			GetBroadPhaseLayer(JPH::ObjectLayer inLayer) const override
+	{
+		JPH_ASSERT(inLayer < Layers::NUM_LAYERS);
+		return mObjectToBroadPhase[inLayer];
+	}
+
+#if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+	virtual const char* GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const override
+	{
+		switch ((BroadPhaseLayer::Type)inLayer)
+		{
+		case (BroadPhaseLayer::Type)BroadPhaseLayers::NON_MOVING:	return "NON_MOVING";
+		case (BroadPhaseLayer::Type)BroadPhaseLayers::MOVING:		return "MOVING";
+		default:													JPH_ASSERT(false); return "INVALID";
+		}
+	}
+#endif // JPH_EXTERNAL_PROFILE || JPH_PROFILE_ENABLED
+
+private:
+	JPH::BroadPhaseLayer					mObjectToBroadPhase[Layers::NUM_LAYERS];
+};
+
+class ObjectVsBroadPhaseLayerFilterImpl : public JPH::ObjectVsBroadPhaseLayerFilter
+{
+public:
+	virtual bool				ShouldCollide(JPH::ObjectLayer inLayer1, JPH::BroadPhaseLayer inLayer2) const override
+	{
+		switch (inLayer1)
+		{
+		case Layers::NON_MOVING:
+			return inLayer2 == BroadPhaseLayers::MOVING;
+		case Layers::MOVING:
+			return true;
+		default:
+			JPH_ASSERT(false);
+			return false;
+		}
+	}
+};
+
+// An example contact listener
+class MyContactListener : public JPH::ContactListener
+{
+public:
+	// See: ContactListener
+	virtual ValidateResult	OnContactValidate(const Body& inBody1, const Body& inBody2, RVec3Arg inBaseOffset, const CollideShapeResult& inCollisionResult) override
+	{
+		std::cout << "Contact validate callback" << std::endl;
+
+		// Allows you to ignore a contact before it is created (using layers to not make objects collide is cheaper!)
+		return ValidateResult::AcceptAllContactsForThisBodyPair;
+	}
+
+	virtual void			OnContactAdded(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings) override
+	{
+		std::cout << "A contact was added" << std::endl;
+	}
+
+	virtual void			OnContactPersisted(const Body& inBody1, const Body& inBody2, const ContactManifold& inManifold, ContactSettings& ioSettings) override
+	{
+		std::cout << "A contact was persisted" << std::endl;
+	}
+
+	virtual void			OnContactRemoved(const SubShapeIDPair& inSubShapePair) override
+	{
+		std::cout << "A contact was removed" << std::endl;
+	}
+};
+
+// An example activation listener
+class MyBodyActivationListener : public BodyActivationListener
+{
+public:
+	virtual void		OnBodyActivated(const BodyID& inBodyID, uint64 inBodyUserData) override
+	{
+		std::cout << "A body got activated" << std::endl;
+	}
+
+	virtual void		OnBodyDeactivated(const BodyID& inBodyID, uint64 inBodyUserData) override
+	{
+		std::cout << "A body went to sleep" << std::endl;
+	}
+};
 
 namespace arc
 {
@@ -52,6 +199,48 @@ namespace arc
 
 	void arc::cFirstApp::run()
 	{
+		/// Jolt Testing Ground
+		JPH::RegisterDefaultAllocator();
+
+		JPH::Factory::sInstance = new JPH::Factory();
+
+		JPH::RegisterTypes();
+
+		JPH::TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
+
+		JPH::JobSystemThreadPool job_system(JPH::cMaxPhysicsJobs, JPH::cMaxPhysicsBarriers, JPH::thread::hardware_concurrency() - 1);
+
+		const JPH::uint cMaxBodies = 1024;
+
+		const JPH::uint cNumBodyMutexes = 0;
+
+		const JPH::uint cMaxBodyPairs = 1024;
+
+		const JPH::uint cMaxContactConstraints = 1024;
+
+		BPLayerInterfaceImpl broad_phase_layer_interface;
+
+		ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter;
+
+		ObjectLayerPairFilterImpl object_vs_object_layer_filter;
+
+		JPH::PhysicsSystem physics_system;
+		physics_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, broad_phase_layer_interface, object_vs_broadphase_layer_filter, object_vs_object_layer_filter);
+
+		MyBodyActivationListener body_activation_listener;
+		physics_system.SetBodyActivationListener(&body_activation_listener);
+
+		MyContactListener contact_listener;
+		physics_system.SetContactListener(&contact_listener);
+
+		BodyInterface& body_interface = physics_system.GetBodyInterface();
+
+		BoxShapeSettings floor_shape_settings(Vec3(100.0f, 1.0f, 100.0f));
+		floor_shape_settings.SetEmbedded();
+
+
+		/// Jolt Testing Ground
+
 		printf("FirstApp Run\n");
 
 		std::vector<std::unique_ptr<cBuffer>> uboBuffers(arcSwapChain::MAX_FRAMES_IN_FLIGHT);
